108d1d5766b3d5551faa4a5ca824e5f2a10e9abd
ugurunal45 <ugur.unal45@icloud.com>
2026-03-11 12:12:15 +0300

    Create test sketch file miyo_bldr_test.ino

---
+++ miyo_bldr_test.ino
@@ -0,0 +1,31 @@
+// miyo_bldr_test.ino
+
+void setup() {
+  Serial.begin(9600);
+  // Display Boot Screen
+  bootScreen();
+}
+
+void loop() {
+  // Call functions for clock, weather, and eye animation
+}
+
+void bootScreen() {
+  // Display "MIYO BLDR" on screen
+}
+
+void eyeAnimation() {
+  // Code for eye animation
+}
+
+void displayClock() {
+  // Code to display current time
+}
+
+void displayWeather() {
+  // Code to fetch and display weather for Manisa using OpenWeatherMap API
+}
+
+void buttonTest() {
+  // Code for TTP223 button testing and serial output
+}
